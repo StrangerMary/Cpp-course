923bc73faf6e8ae1d23f516fb6234e04de4f1249
StrangerMary <35704850+StrangerMary@users.noreply.github.com>
2018-01-28 01:01:00 +0200

    Add files via upload

---
+++ c++/will be destroyed/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<"Остача від ділення:"<<"на 2 ="<<a%2<<", на 3 ="<<a%3<<", на 5 ="<<a%5<<endl;
+    return 0;
+}
